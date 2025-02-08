84fdb0c32d5e8b6f3d7f47bf8192322803d72faa
Jonathan20061613 <andressguilera2006@gmail.com>
2025-02-08 16:05:22 -0500

    configuracion Backend

--- Backend/app.js
+++ Backend/app.js
@@ -8,11 +8,17 @@ import loginAdminRouter from "./src/routes/loginAdmin.routes.js";
 import loginUserRouter from "./src/routes/loginUser.routes.js";
 import cors from "cors";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
 const app = express();
 dotenv.config();
 connectionMongo();
 
 app.use(cors());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use("/usuarios", routerUser);
@@ -20,9 +26,14 @@ app.use("/admin", routerAdmin);
 app.use("/mascotas", petsrouter);
 app.use("/loginAdmin", loginAdminRouter);
 app.use("/loginUser", loginUserRouter);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+// Ruta principal para servir index.html
 app.get("/", (req, res) => {
-  res.send("Hola mundo, Backend Funcionando Correctamente");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
 
 export default app;
 
